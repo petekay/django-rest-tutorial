878841d41227f3751101f86c396e391c3075e380
petekay <pkitowski@hotmail.de>
2020-11-09 20:21:30 +0100

    Tut 2 - Generic class-based views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,41 +1,20 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+"""
+Using generic class-based views
+
+Using the mixin classes we've rewritten the views to use slightly less code than before, but we can go one step further. 
+REST framework provides a set of already mixed-in generic views that we can use to trim down our views.py module even more.
+
+"""
+
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
